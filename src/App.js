3a43ff0defc10f72dbf0b56bf74c5158761bec69
Henish Patel <henish134@gmail.com>
2022-02-15 15:52:08 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Movies from "./components/Movies";
 import Pagination from "./components/Pagination";
+import Favourites from "./components/Favourites";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <NavBar></NavBar>
-            <Banner></Banner>
-            <Movies></Movies>
-            <Pagination></Pagination>
-        </>
+            <Routes>
+                <Route path="/" element={<>
+                    <Banner/>
+                    <Movies />
+                    <Pagination/>
+                </>}></Route>
+                <Route path="/favourites" element={<Favourites/>}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Logo from "../navbar-logo.png";
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <>
             <div className="border pl-12 py-4 space-x-8 flex items-center">
                 <img className="w-[30px] md:w-[50px]" src={Logo}></img>
-                <div className="text-blue-400 font-bold text-xl md:text-3xl">Movies</div>
-                <div className="text-blue-400 font-bold text-xl md:text-3xl">
+                <Link to="/" className="text-blue-400 font-bold text-xl md:text-3xl">
+                    Movies
+                </Link>
+                <Link to="favourites" className="text-blue-400 font-bold text-xl md:text-3xl">
                     Favourites
-                </div>
+                </Link>
             </div>
         </>
     );
