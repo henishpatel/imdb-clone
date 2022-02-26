7811cc4f760396e13914e5ba6a63258641dfb714
Henish Patel <henish134@gmail.com>
2022-02-26 13:24:50 +0530

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Movies from "./components/Movies";
-import Pagination from "./components/Pagination";
 import Favourites from "./components/Favourites";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
